f726c80f21533fe859e5de34e34740d71b674dfa
vivaiobice <vivaiobice@gmail.com>
2026-08-25 11:22:49 +0200

    Add files via upload
    Versione quattro

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='art-racing-v3';const ASSETS=['./','./index.html','./race.html','./regolamento.html','./styles.css','./data.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='art-racing-v5';
+const ASSETS=['./','./index.html','./race.html','./driver.html','./regolamento.html','./styles.css','./data.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
