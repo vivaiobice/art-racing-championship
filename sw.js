fc5189ee18a2e7f7a1eb3dbf599e860d3eb1fb14
vivaiobice <vivaiobice@gmail.com>
2026-08-25 11:53:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='art-racing-v7';
+const CACHE='art-racing-v8';
 const ASSETS=['./','./index.html','./race.html','./driver.html','./regolamento.html','./styles.css','./data.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
